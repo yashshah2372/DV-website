d113b0ee32c4cba6fa9959e03d7af0d9e18a5063
Yash Shah <yashromilshah@gmail.com>
2022-08-14 13:46:07 +0530

    featured/backend and frontend

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ var engine = require('consolidate');
 //Return main object of epress functionality
 const app = express();
 const PORT = process.env.PORT || 3000
+const upload = require("express-fileupload");
+const AWS = require("aws-sdk");
+app.use(upload());
+
+
+const s3 = new AWS.S3({
+    accessKeyId: 'AKIA4MUVW5KQCYWBALW4',// your AWS access id
+    secretAccessKey: 'WmXXuD5L/rWO6fy6o+tDIxFwKGLoVqBR4qwAY8pp', // your AWS access key
+  });
+
+  async function uploadFile(file,folderName) {
+      const params = {
+        Bucket: 'profile-picture-uploader', // bucket you want to upload to
+        Key: `${folderName}/-${Date.now()}.png`, // put all image to fileupload folder with name scanskill-${Date.now()}${file.name}`
+        Body: file.data,
+        //ACL: "public-read",
+      };
+      const data = await s3.upload(params).promise();
+      return data.Location; // returns the url location
+    }
 
 const mongoose = require('mongoose');
 const url = 'mongodb+srv://aisa-student:%40isaStudent123@cluster0.k7noaqd.mongodb.net/?retryWrites=true&w=majority';
@@ -28,15 +48,20 @@ app.use(cookieParser());
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/index.html"))
 })
-const User = require('./public/js/userModel')
+const User = require('./public/js/userModel');
+const { db } = require("./public/js/userModel");
 app.post('/postregister',async (req,res)=>{
+    //console.log(req.files.formFile)
+    //console.log(result);
+    let result = await uploadFile(req.files.formFile,"externalimages");
+    const formFile = result;
     const { no_par, t_name, name1, name2, name3, name4, email1, c_no1, clg1, year1, branch1,
         email2, c_no2, clg2, year2, branch2,
         email3, c_no3, clg3, year3, branch3,
         email4, c_no4, clg4, year4, branch4 } = req.body
-        console.log(req.body)
-    //Validate Request
-    if( !no_par || !t_name || !name1 || !name2 || !name3 || !name4 || !email1 || !c_no1 || !clg1|| !year1 || !branch1 || !email2 || !c_no2 || !clg2|| !year2 || !branch2 || !email3 || !c_no3 || !clg3|| !year3 || !branch3 || !email4 || !c_no4 || !clg4|| !year4 || !branch4 ){
+    //     console.log(req.body)
+    // //Validate Request
+    if( !no_par || !t_name || !name1 || !name2 || !name3 || !email1 || !c_no1 || !clg1|| !year1 || !branch1 || !email2 || !c_no2 || !clg2|| !year2 || !branch2 || !email3 || !c_no3 || !clg3|| !year3 || !branch3 ){
         // req.flash('error', 'ALl fields are required')
         console.log('ALl fields are required')
         return res.redirect('/register')
@@ -45,23 +70,26 @@ app.post('/postregister',async (req,res)=>{
     User.exists({ t_name: t_name}, (err,result)=>{
         if(result){
             // req.flash('error', 'Team name Already taken!')
-            console.log('Team name Already taken!')
-            return res.redirect('/register')
+            res.send("Team already taken")
+        }
+        else{
+            const user = new User({
+                no_par, t_name, formFile, name1, name2, name3, name4, email1, c_no1, clg1, year1, branch1,email2, c_no2, clg2, year2, branch2,email3, c_no3, clg3, year3, branch3,email4, c_no4, clg4, year4, branch4
+            })
+            user.save().then((user)=>{
+                //login
+                return res.redirect('/')
+            }).catch(err =>{
+                res.send('Please check all the fields again, email should be unique')
+            })
         }
     })
-    const user = new User({
-        no_par, t_name, name1, name2, name3, name4, email1, c_no1, clg1, year1, branch1,email2, c_no2, clg2, year2, branch2,email3, c_no3, clg3, year3, branch3,email4, c_no4, clg4, year4, branch4
-    })
-    user.save().then((user)=>{
-        //login
-        return res.redirect('/')
-    }).catch(err =>{
-        console.log(err);
-        return res.redirect('/')
-    })
-    // res.render('auth/register')
-    // console.log(req.body)
-    // res.status(404).json({user });
+    
+    
+    // const user = new User({
+    //     formFile
+    // })
+    
 })
 
 const server = app.listen(PORT, ()=>{
